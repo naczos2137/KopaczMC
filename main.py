2dc623c51823eee9e1715c657881fac74a907a29
naczos2137 <detektyw.naczos@gmail.com>
2024-08-31 11:32:48 +0200

    First working version!

---
+++ main.py
@@ -0,0 +1,73 @@
+import concurrent.futures
+import os.path
+import window_control
+import tkinter as tk
+from tkinter import ttk, font
+from PIL import Image, ImageTk
+
+
+bots = {}  # title: window_control
+
+executor = concurrent.futures.ThreadPoolExecutor()
+
+
+def create_new_bot(title):
+    global bots, horizontal_blocks, vertical_blocks, bots_table
+    bot = window_control.Window(title, 'k', horizontal_blocks=int(horizontal_blocks.get()), vertical_blocks=int(vertical_blocks.get()))
+    bots[title] = bot
+    bot.thread = executor.submit(bot.mine)
+
+    mc_title = tk.Label(bots_table, text=title)
+    mc_title.grid(row=bots_table.grid_size()[1], column=0, padx=5, pady=5)
+
+    mc_stop_button = tk.Button(bots_table, text='Stop', command=lambda : stop_bot(title, mc_title, mc_stop_button))
+    mc_stop_button.grid(row=bots_table.grid_size()[1]-1, column=1, padx=5, pady=5)
+
+
+def stop_bot(title, mc_title, mc_stop_button):
+    bots.pop(title).active = False
+    mc_title.destroy()
+    mc_stop_button.destroy()
+
+
+
+root = tk.Tk()
+root.title("KopaczMC")
+root.geometry("800x600")
+root.resizable(False, False)
+
+bg_file = Image.open(os.path.join('assets', 'bg.png'))
+background_image = ImageTk.PhotoImage(bg_file)
+
+canvas = tk.Canvas(root, width=bg_file.width, height=bg_file.height)
+canvas.pack(fill="both", expand=True)
+canvas.create_image(0, 0, image=background_image, anchor="nw")
+
+windows = window_control.list_of_windows()
+selected_window = tk.StringVar()
+dropdown_windows = ttk.Combobox(root, textvariable=selected_window, values=windows, width=30)
+dropdown_windows.place(x=190, y=38)
+
+horizontal_blocks = tk.StringVar()
+horizontal_blocks_input = tk.Entry(root, textvariable=horizontal_blocks, width=5)
+horizontal_blocks_input.place(x=190, y=72)
+
+vertical_blocks = tk.StringVar()
+vertical_blocks_input = tk.Entry(root, textvariable=vertical_blocks, width=5)
+vertical_blocks_input.place(x=250, y=72)
+
+button_create_new_bot = ttk.Button(root, text="Dodaj Bota", command=lambda: create_new_bot(selected_window.get()))
+button_create_new_bot.place(x=150, y=250)
+
+data = [("Element 1", "Akcja 1"), ("Element 2", "Akcja 2"), ("Element 3", "Akcja 3")]
+
+bots_table = tk.Frame(root)
+bots_table.place(x=450, y=40)
+
+
+root.mainloop()
+
+executor.shutdown(wait=False)
+
+for window in bots.values():
+    window.active = False
---
+++ window_control.py
@@ -0,0 +1,48 @@
+import os.path
+import time
+from ahk import AHK
+
+
+def list_of_windows():
+    windows = AHK(executable_path=os.path.join('assets', 'AutoHotkey.exe')).windows()
+    windows_titles = [window.title for window in windows if len(window.title) > 0]
+    return windows_titles
+
+
+class Window:
+    def __init__(self, window_title, mine_button, horizontal_blocks=5, vertical_blocks=1):
+        self.mine_button = mine_button
+        self.horizontal_blocks = (horizontal_blocks - 1) / 3
+        self.vertical_blocks = (vertical_blocks - 1) / 3
+        self.active = True
+        ahk = AHK(executable_path=os.path.join('assets', 'AutoHotkey.exe'))
+        self.win = ahk.find_window(title=window_title)
+        self.visible = True
+        self.thread = None
+
+    def change_visible(self):
+        if self.visible:
+            self.win.hide()
+            self.visible = False
+        else:
+            self.win.show()
+            self.visible = True
+
+    def mine(self):
+        self.win.send('{' + self.mine_button + ' down}')
+        while self.active:
+            self.win.send('{d down}')
+            time.sleep(self.horizontal_blocks)
+            self.win.send('{d up}')
+            self.win.send('{s down}')
+            time.sleep(self.vertical_blocks)
+            self.win.send('{s up}')
+            self.win.send('{a down}')
+            time.sleep(self.horizontal_blocks)
+            self.win.send('{a up}')
+            self.win.send('{w down}')
+            time.sleep(self.vertical_blocks)
+            self.win.send('{w up}')
+        self.win.send('{' + self.mine_button + ' up}')
+
+
